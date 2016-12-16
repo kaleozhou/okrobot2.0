1140099ee4a1f0ff1049315aded651da84653351
kaleo <kaleo1990@hotmail.com>
2016-12-16 09:02:04 +0800

    home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,9 +27,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         $login_user=$request->user();
-        //读取配置文件的key
         if ($login_user->api_key!=null&&$login_user->secret_key!=null) {
-            
         $OKTOOL=new OKTOOL($login_user);
         $res=$OKTOOL->update_data_database();
         $res=$OKTOOL->autotrade();
