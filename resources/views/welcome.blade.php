53294be01755d1ff6f5d7cc9ac8d3ce3dfcc9b5f
kaleo <kaleo1990@hotmail.com>
2016-12-16 12:16:02 +0800

    da

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,25 +69,22 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/home') }}">个人中心</a>
                     @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
+                        <a href="{{ url('/login') }}">登录</a>
+                        <a href="{{ url('/register') }}">注册</a>
                     @endif
                 </div>
             @endif
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    KALEOBTC自动交易策略平台
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/home">OKCoin 自动交易机器人</a>
+                    <a href="/">策略</a>
                 </div>
             </div>
         </div>
