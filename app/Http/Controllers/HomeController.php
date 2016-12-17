f50550cb18d8a83936fb3fc08f6f984c10b3567a
kaleo <kaleo1990@hotmail.com>
2016-12-17 12:40:00 +0800

    zidong

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,7 @@ class HomeController extends Controller
     public function index(Request $request)
     {
         if ($request->user()->api_key==null||$request->user()->secret_key==null) {
-        $this->error='请设置您的api_key和secret_key';
+            $this->error='请设置您的api_key和secret_key';
         }
         //$login_user->api_key=config('okcoin.api_key');
         //$login_user->secret_key=config('okcoin.secret_key');
@@ -44,7 +44,17 @@ class HomeController extends Controller
             ->simplePaginate(5);
         return view('home',['userinfo'=>$newuserinfo,'ticker'=>$newticker,'set'=>$newset,'orderinfos'=>$orderinfos,'user'=>$login_user,'error'=>$this->error]);
     }
+    /**
+     *设置开始自动交易
+     * @access 
+     * @author kaleo <kaleo1990@hotmail.com>
+     * @param
+     * @return
+     */
     public function starttrade(Request $request){
+        if ($request->user()->api_key==null||$request->user()->secret_key==null) {
+            $this->error='请设置您的api_key和secret_key';
+        }
         $login_user=$request->user();
         if ($login_user->api_key!=null&&$login_user->secret_key!=null)
         {
@@ -61,7 +71,17 @@ class HomeController extends Controller
             ->simplePaginate(5);
         return view('home',['userinfo'=>$newuserinfo,'ticker'=>$newticker,'set'=>$newset,'orderinfos'=>$orderinfos,'user'=>$login_user,'error'=>$this->error]);
     }
+    /**
+     * 设置关闭自动交易
+     * @access 
+     * @author kaleo <kaleo1990@hotmail.com>
+     * @param
+     * @return
+     */
     public function stoptrade(Request $request){
+        if ($request->user()->api_key==null||$request->user()->secret_key==null) {
+            $this->error='请设置您的api_key和secret_key';
+        }
         $login_user=$request->user();
         $login_user->autotrade=false;
         $login_user->save();
@@ -76,32 +96,4 @@ class HomeController extends Controller
         return view('home',['userinfo'=>$newuserinfo,'ticker'=>$newticker,'set'=>$newset,'orderinfos'=>$orderinfos,'user'=>$login_user,'error'=>$this->error]);
     }
 
-    /**
-     * @access public 
-     * @author kaleo <kaleo1990@hotmail.com>
-     * @param  $user 传入用户模型
-     * @return 
-     */
-    public function autotrade($user){
-        //检测是否设置api_key
-        try{
-            if ($user->api_key!=null&&$user->secret_key!=null)
-            {
-                $OKTOOL=new OKTOOL($user);
-                $res=$OKTOOL->update_data_database();
-                $res=$OKTOOL->autotrade();
-            }
-            else
-            {
-                return false;
-                var_dump('请设置你的api_key和secret_key!');
-            }
-        }
-        catch(exception $e){
-            //修改设置
-            $user->autotrade=false;
-            $user->save();
-            return false;
-        }
-    }
 }
