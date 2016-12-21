ee7288c4d95ce0a33643389d9a748dcc16e90b49
kaleo <kaleo1990@hotmail.com>
2016-12-21 14:04:47 +0800

    kaleo

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -15,9 +15,7 @@ require('./bootstrap');
 
 Vue.component('example', require('./components/Example.vue'));
 const app=new Vue({
-    el: '#app',
-        data:{
-        }
+    el: '#app'
 });
 Vue.component(
 'passport-clients',
