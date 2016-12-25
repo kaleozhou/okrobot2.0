62b3913d103b1c7bda6e07dda3d6b86aa653ce3e
kaleo <kaleo1990@hotmail.com>
2016-12-25 15:24:59 +0800

    kaleo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,139 +14,121 @@ setTimeout('myrefresh()', 5000); //指定1秒刷新一次
             <div class="panel panel-primary">
                 <div class="panel-heading">
                     <div class='row'>
-                    <label class="lead col-xs-4">控制面板</label>
+                        <label class="lead col-xs-4">控制面板</label>
                     </div>
                     <div class='row'>
-                    @if ($profit>0)
-                    <div class='col-xs-4'>
-                        利润:
-                        <span class="label label-success lead">{{round($profit,2)}}%</span></div>
-                    @else
-                    <div class='col-xs-4'>
-                        利润:
-                        <span class="label label-warning lead">{{round($profit,2)}}</span></div>
-                    @endif
-                    <div class='col-xs-8'>
-                    更新:
-                    <span>{{$userinfo->updated_at}}</span> 
+                        @if ($profit>0)
+                        <div class='col-xs-4 col-lg-2'>
+                            利润:
+                            <span class="label label-success lead">{{round($profit,2)}}%</span></div>
+                        @else
+                        <div class='col-xs-4 col-lg-2'>
+                            利润:
+                            <span class="label label-warning lead">{{round($profit,2)}}</span></div>
+                        @endif
+                        <div class='col-xs-8 col-lg-4'>
+                            更新:
+                            <span>{{$userinfo->updated_at}}</span> 
+                        </div>
+                        @if ($userinfo!=null)
+                        @if ($user->btc_autotrade=='1')
+                        <div class='col-xs-6 col-lg-3'>
+                            BTC状态:<span class="label label-success ">BTC运行中</span></div>
+                        @else
+                        <div class='col-xs-6 col-lg-3'>
+                            BTC状态:<span class="label label-danger ">BTC已停止</span></div>
+                        @endif 
+                        @if ($user->ltc_autotrade=='1')
+                        <div class='col-xs-6 col-lg-3'>
+                            LTC状态:<span class="label label-success ">LTC运行中</span></div>
+                        @else
+                        <div class='col-xs-6 col-lg-3'>
+                            LTC状态:<span class="label label-danger ">LTC已停止</span></div>
+                        @endif 
+                        @endif
+                        <div class='col-xs-6 col-lg-6'>
+                            <a class="btn btn-success btc-mini"href='/starttrade/btc_cny'>BTC开始</a>
+                            <a class="btn btn-danger btc-mini"href='/stoptrade/btc_cny'>BTC停止</a> 
+                        </div>
+                        <div class='col-xs-6 col-lg-6'>
+                            <a class="btn btn-success btc-mini"href='/starttrade/ltc_cny'>LTC开始</a>
+                            <a class="btn btn-danger btc-mini"href='/stoptrade/ltc_cny'>LTC停止</a> 
+                        </div>
+                        @if ($error!=false)
+                        <div class='col-xs-12'>
+                            <label>
+                                {{$error}}
+                            </label> @endif &nbsp;&nbsp;&nbsp;&nbsp; 
+                        </div>
                     </div>
                 </div>
-                    @if ($userinfo!=null)
-                    <div class='row'>
-                    @if ($user->btc_autotrade=='1')
-                    <div class='col-xs-6'>
-                    BTC状态：<span class="label label-success ">BTC运行中</span></div>
-                    @else
-                    <div class='col-xs-6'>
-                    BTC状态：<span class="label label-danger ">BTC已停止</span></div>
-                    @endif 
-                    @if ($user->ltc_autotrade=='1')
-                    <div class='col-xs-6'>
-                    LTC状态：<span class="label label-success ">LTC运行中</span></div>
-                    @else
-                    <div class='col-xs-6'>
-                    LTC状态：<span class="label label-danger ">LTC已停止</span></div>
-                    @endif 
-                    </div>
-                    @endif
-                    <div class='row'>
-                    <div class='col-xs-6'>
-                    <a class="btn btn-success"href='/starttrade/btc_cny'>BTC开始</a>
-                    <a class="btn btn-danger"href='/stoptrade/btc_cny'>BTC停止</a> 
-                    </div>
-                    <div class='col-xs-6'>
-                    <a class="btn btn-success"href='/starttrade/ltc_cny'>LTC开始</a>
-                    <a class="btn btn-danger"href='/stoptrade/ltc_cny'>LTC停止</a> 
-                    </div>
-                    </div>
-                    @if ($error!=false)
-                    <div class='row'>
-                    <div class='col-xs-12'>
-                    <label>
-                        {{$error}}
-                    </label> @endif &nbsp;&nbsp;&nbsp;&nbsp; 
-                    </div>
-                    </div>
-                    </div>
                 <div class="panel-body">
                     <h4>账户信息</h4>
-                    <table class="table table-bordered">
-                        @if ($userinfo!=null)
-                        <tr class='row'>
-                            <td class='col-xs-3'>余额：</td>
-                            <td class='col-xs-3'>{{$userinfo->asset_net}}</td>
-                            <td class='col-xs-3'>总额：</td>
-                            <td class='col-xs-3'>{{$userinfo->asset_total}}</td>
-                        </tr>
-                        <tr class='row'>
-                            <td class='col-xs-2'>可用BTC：</td>
-                            <td class='col-xs-2'>{{$userinfo->free_btc}}</td>
-                            <td class='col-xs-2'>可用LTC：</td>
-                            <td class='col-xs-2'>{{$userinfo->free_ltc}}</td>
-                        </tr>
-                        <tr class='row'>
-                            <td class='col-xs-2'>可用资金：</td>
-                            <td class='col-xs-10'>{{$userinfo->free_cny}}</td>
-                        </tr>
-                        @endif
-                        @if ($btc_ticker!=null&&$set!=null)
-                        <tr class='row'>
-                            <td class='col-xs-3'>BTC价格：</td>
-                            <td class='col-xs-3'>{{$btc_ticker->last_price}}</td>
-                            <td class='col-xs-3'>上次价格：</td>
-                            <td class='col-xs-3'>{{$set->btc_my_last_price}}</td>
-                        </tr>
-                        <tr class='row'>
-                            <td class='col-xs-2'>价值波动：</td>
-                            <td class='col-xs-2'>{{$set->btc_n_price}}</td>
-                            <td class='col-xs-2'>差价：</td>
-                            <td class='col-xs-2'>{{round($btc_ticker->last_price-$set->btc_my_last_price,2)}}</td>
-                        </tr>
-                        <tr class='row'>
-                            <td class='col-xs-2'>偏差：</td>
-                            <td class='col-xs-2'>{{$set->btc_n_price*$set->uprate}}</td>
-                        </tr>
-                        @endif
-                        @if ($ltc_ticker!=null&&$set!=null)
-                        <tr class='row'>
-                            <td class='col-xs-3'>LTC价格：</td>
-                            <td class='col-xs-3'>{{$ltc_ticker->last_price}}</td>
-                            <td class='col-xs-3'>上次价格：</td>
-                            <td class='col-xs-3'>{{$set->ltc_my_last_price}}</td>
-                        </tr>
-                        <tr class='row'>
-                            <td class='col-xs-2'>价值波动：</td>
-                            <td class='col-xs-2'>{{$set->ltc_n_price}}</td>
-                            <td class='col-xs-2'>差价：</td>
-                            <td class='col-xs-2'>{{round($ltc_ticker->last_price-$set->ltc_my_last_price,2)}}</td>
-                        </tr>
-                        <tr class='row'>
-                            <td class='col-xs-2'>偏差：</td>
-                            <td class='col-xs-2'>{{$set->ltc_n_price*$set->uprate}}</td>
-                        </tr>
-                        @endif
-                    </table>
+                    @if ($userinfo!=null)
+                    <div class='row'>
+                        <span class='col-xs-3 col-lg-1'>余额:</span>
+                        <span class='col-xs-3 col-lg-2 numeric'>{{$userinfo->asset_net}}</span>
+                        <span class='col-xs-3 col-lg-1'>总额:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$userinfo->asset_total}}</span>
+                        <span class='col-xs-3 col-lg-1'>BTC:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$userinfo->free_btc}}</span>
+                        <span class='col-xs-3 col-lg-1'>LTC:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$userinfo->free_ltc}}</span>
+                        <span class='col-xs-6 col-lg-2 text-warning'>资金:</span>
+                        <span class='col-xs-6 col-lg-1 text-warning'>{{$userinfo->free_cny}}</span>
+                    </div>
+                    @endif
+                    @if ($btc_ticker!=null&&$set!=null)
+                    <div class='row'>
+                        <span class='col-xs-3 col-lg-1'>BTC:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$btc_ticker->last_price}}</span>
+                        <span class='col-xs-3 col-lg-1'>前价:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$set->btc_my_last_price}}</span>
+                        <span class='col-xs-3 col-lg-1'>波动:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$set->btc_n_price}}</span>
+                        <span class='col-xs-3 col-lg-1'>差价:</span>
+                        <span class='col-xs-3 col-lg-2'>{{round($btc_ticker->last_price-$set->btc_my_last_price,3)}}</span>
+                        <span class='col-xs-6 col-lg-2 text-warning'>偏差:</span>
+                        <span class='col-xs-6 col-lg-1 text-warning '>{{$set->btc_n_price*$set->uprate}}</span>
+                    </div>
+                    @endif
+                    @if ($ltc_ticker!=null&&$set!=null)
+                    <div class='row'>
+                        <span class='col-xs-3 col-lg-1'>LTC:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$ltc_ticker->last_price}}</span>
+                        <span class='col-xs-3 col-lg-1'>前价:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$set->ltc_my_last_price}}</span>
+                        <span class='col-xs-3 col-lg-1'>波动:</span>
+                        <span class='col-xs-3 col-lg-2'>{{$set->ltc_n_price}}</span>
+                        <span class='col-xs-3 col-lg-1'>差价:</span>
+                        <span class='col-xs-3 col-lg-2'>{{round($ltc_ticker->last_price-$set->ltc_my_last_price,2)}}</span>
+                        <span class='col-xs-6 col-lg-2 text-warning'>偏差:</span>
+                        <span class='col-xs-6 col-lg-1 text-warning'>{{$set->ltc_n_price*$set->uprate}}</span>
+                    </div>
+                    @endif
                     <h4>我的订单</h4>
-                    <table class="table table-bordered">
-                    @if ($orderinfos!=null)
-                    @foreach ($orderinfos as $orderinfo)
+                    <table class="table table-bordered table-container ">
                         <tr>
-                            <td>成交价格</td>
+                            <td class='lead'>价格</td>
+                            <td class='lead'>数量</td>
+                            <td class='lead'>类型</td>
+                            <td class='lead'>类型</td>
+                            <td class='lead'>时间</td>
+                        </tr>
+                        @if ($orderinfos!=null)
+                        <tr>
+                            @foreach ($orderinfos as $orderinfo)
                             <td>{{$orderinfo->avg_price}}</td>
-                            <td>成交量</td>
                             <td>{{$orderinfo->deal_amount}}</td>
-                            <td>类型</td>
                             <td>{{$orderinfo->symbol}}</td>
-                            <td>订单类型</td>
                             @if ($orderinfo->ordertype=='buy_market')
                             <td>买入</td>
                             @else
                             <td>卖出</td>
                             @endif
-                            <td>时间</td>
                             <td>{{$orderinfo->create_date}}</td>
-                            <tr>
-                                @endforeach
+                        </tr>
+                        @endforeach
                     </table>
                     @endif
                 </div>
