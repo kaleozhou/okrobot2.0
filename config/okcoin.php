8dce2eff407ccac9734bb3202499dabfb4dabef5
kaleo <kaleo1990@hotmail.com>
2016-12-26 09:44:40 +0800

    kaleo

--- config/okcoin.php
+++ config/okcoin.php
@@ -11,9 +11,9 @@ return[
 'secret_key'=> "461D47D0FE52B28288E1285D8D899812",
 'downline'=>6000,//初始化止损值
 'upline'=>100000,//止盈值
-'uprate'=>0.5,//上浮率
-'downrate'=>0.5,//下浮动率
-'unit'=>0.2,//下单单位
+'uprate'=>1,//上浮率
+'downrate'=>2,//下浮动率
+'unit'=>0.5,//下单单位
 'unitrate'=>0.5,//买入，卖出对价值波动的比率
 'klinetype'=>"5min",//kline的周期
 'smsusername'=>"kaleozhou",//短信用户名
