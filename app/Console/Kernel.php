d23ed68f9e2b4866b8dcd553ef5dad95de923694
kaleo <kaleo1990@hotmail.com>
2016-12-27 14:41:07 +0800

    kaleo

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -130,7 +130,7 @@ class Kernel extends ConsoleKernel
                                 break;
                             }
                             $newuserinfo=$OKTOOL->get_new_info('userinfo',$symbol);
-                            Log::info('tradetype:'.$tradetype.'-'.$symbol.'-: '.$user->name.'-cost'.$user->cost.' asset_net: '.$newuserinfo->asset_net.' asset_total: '.$newuserinfo->asset_total);
+                            Log::info('tradetype:'.$tradetype.'-'.$symbol.'-:'.$user->name.'-cost'.$user->cost.'-asset_net:'.$newuserinfo->asset_net.'-asset_total: '.$newuserinfo->asset_total);
                         }
                         else
                         {
