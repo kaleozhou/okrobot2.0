a4225278419e11f452a00ed56274e092a94099ef
kaleo <kaleo1990@hotmail.com>
2016-12-28 09:02:14 +0800

    kaleo

--- config/okcoin.php
+++ config/okcoin.php
@@ -12,7 +12,7 @@ return[
     'upline'=>100000,//止盈值
     'uprate'=>0.4,//上浮率
     'downrate'=>0.4,//下浮动率
-    'unit'=>0.33,//下单单位
+    'unit'=>0.2,//下单单位
     'klinetype'=>"3min",//kline的周期
     'tradetype'=>2,//使用策略类型，1，2
     //未使用参数
