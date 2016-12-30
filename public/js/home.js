1bce0020934a306c160b4df6c3b6549b053dfed2
kaleo <kaleo1990@hotmail.com>
2016-12-30 08:28:58 +0800

    kaleo

--- public/js/home.js
+++ public/js/home.js
@@ -1,4 +1,4 @@
-window.setInterval(refresh,3000);
+window.setInterval(refresh,1000);
 
 function refresh(){
         $.ajax({
@@ -10,16 +10,17 @@ function refresh(){
                 'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
             },
             success: function(data){
+                document.getElementById("profit").innerText =data.profit+"%";
                 document.getElementById("asset_net").innerText = data.userinfo.asset_net;
                 document.getElementById("free_btc").innerText = data.userinfo.free_btc;
                 document.getElementById("free_ltc").innerText = data.userinfo.free_ltc;
                 document.getElementById("free_cny").innerText = data.userinfo.free_cny;
-                document.getElementById("zhuanle").innerText = round((data.userinfo.asset_total-data.user.cost),2);
-                document.getElementById("btc_last_price").innerText = data.btc_ticker.last_last_price;
-                document.getElementById("profit").innerText =data.profit+"%";
+                document.getElementById("zhuanle").innerText = "盈利："+(data.userinfo.asset_total-data.user.cost).toFixed(2);
+                document.getElementById("updated_at").innerText = data.userinfo.updated_at;
+//                document.getElementById("btc_last_price").innerText = data.btc_ticker.last_last_price;
             },
             error: function(xhr, type){
-                alert('Ajax error!')
+//                alert('Ajax error!')
             }
         });
 }
